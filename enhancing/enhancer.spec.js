77ec6b6fd59c4fb59d3ea6415595412ddc640363
Amber-Pittman <amber@amberpittman.com>
2020-05-14 20:11:07 -0400

    Working on enhancer tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,25 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+const item = {
+    name: "Amber",
+    durability: 80,
+    enhancement: 19
+}
+
+describe("Enhancer Testing", () => {
+    it("repair()", () => {
+        expect(enhancer.repair(item).durability).toEqual(100)
+    })
+
+    it("success()", () => {
+        const newItem = enhancer.succeed(item)
+        const ogEnhancement = item.enhancement
+
+        if (item.enhancement === 20) {
+            expect(newItem.enhancement).toEqual(ogEnhancement)
+        } else {
+            expect(newItem.enhancement).toEqual(ogEnhancement + 1)
+        }
+    })
+})
